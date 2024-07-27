46bf6af473e31b7edb400a24449fcd2ac861971a
Dhanush <dhanushsai2005@gmail.com>
2024-07-27 19:32:23 +0530

    WAP to find the min no of Days to make 'm' bouquets and each bouquet consisting 'k' flowers

---
+++ Binary_Search/BS_on_Answers/04_MinDayForMBouquets/02_optimalApproach.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool possible(vector<int> &bloomDay, int day, int m, int k) {
+    int n = bloomDay.size(); 
+    int cnt = 0;
+    int noOfB = 0;
+    for (int i = 0; i < n; i++) {
+        if (bloomDay[i] <= day) {
+            cnt++;
+        }
+        else {
+            noOfB += (cnt / k);
+            cnt = 0;
+        }
+    }
+    noOfB += (cnt / k);
+    return noOfB >= m;
+}
+
+int minDay(vector<int>& bloomDay, int m, int k) {
+    long long val = m * 1ll * k * 1ll;
+    int n = bloomDay.size();
+    if (val > n) return -1;
+
+    int minn = *min_element(bloomDay.begin(), bloomDay.end());
+    int maxi = *max_element(bloomDay.begin(), bloomDay.end());
+
+    int low = minn, high = maxi;
+
+    while(low <= high){
+        int mid = low + ((high - low)/2);
+
+        if(possible(bloomDay, mid, m, k)){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
+
+int main()
+{
+    vector<int> bloomDay = {7, 7, 7, 7, 13, 11, 12, 7};
+    int k = 3;
+    int m = 2;
+    int ans = minDay(bloomDay, k, m);
+    if (ans == -1)
+        cout << "We cannot make m bouquets.\n";
+    else
+        cout << "We can make bouquets on day " << ans << "\n";
+    return 0;
+}
