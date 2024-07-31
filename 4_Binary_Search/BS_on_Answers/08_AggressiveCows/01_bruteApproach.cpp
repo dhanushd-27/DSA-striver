253d21b91e7c7e7816906b82a7ccaaf30f3eb40e
Dhanush <dhanushsai2005@gmail.com>
2024-07-31 11:31:05 +0530

    Modified the brute approach of solving Aggressive Cows Problem

--- 4_Binary_Search/BS_on_Answers/08_AggressiveCows/01_bruteApproach.cpp
+++ 4_Binary_Search/BS_on_Answers/08_AggressiveCows/01_bruteApproach.cpp
@@ -3,33 +3,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool possible(vector<int> stalls, int cows, int dist){
+bool possible(vector<int>& stalls, int dist, int cows){
     int n = stalls.size();
-    int cntCows = 1, last = stalls[0];
+    int cntCows = 1;
+    int last = stalls[0];
 
-    for(int i = 1; i < n; i++){
-        if(stalls[1] - last >= dist){
+    for(int i = 0; i < n; i++){
+        if((stalls[i] - last) >= dist){
             cntCows++;
             last = stalls[i];
         }
 
-        if(cntCows >= cows) return true;
+        if(cntCows >= cows){
+            return true;
+        }
     }
 
     return false;
 }
 
-int aggressiveCows(vector<int> &stalls, int k) {
-    int n = stalls.size(); //size of array
-    //sort the stalls[]:
+int aggressiveCows(vector<int>& stalls, int cows){
+    int n = stalls.size();
     sort(stalls.begin(), stalls.end());
+    int limit = stalls[n-1] - stalls[0];
 
-    int limit = stalls[n - 1] - stalls[0];
-    for (int i = 1; i <= limit; i++) {
-        if (possible(stalls, k, i) == false) {
-            return (i - 1);
+    for(int dist = 1; dist <= limit; dist++){
+        if(possible(stalls, dist, cows) == false){
+            return dist - 1;
         }
     }
+
     return limit;
 }
 
