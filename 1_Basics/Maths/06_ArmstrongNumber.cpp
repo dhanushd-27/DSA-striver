1e247e1d4fe48df6a1287f5e3727ff96514336dc
Dhanush D <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-08-24 19:33:42 +0530

    WAP to find whether given number is Armstrong or Not

---
+++ 1_Basics/Maths/06_ArmstrongNumber.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isArmstrong(int number){
+    // Calculate the number of
+    // digits in the given number
+    int k = to_string(number).length();
+    // Initialize the sum of digits
+    // raised to the power of k to 0
+    int sum = 0;
+    // Copy the value of the input
+    // number to a temporary variable n
+    int n = number;
+    // Iterate through each
+    // digit of the number
+    while(n > 0){
+        // Extract the last
+        // digit of the number
+        int ld = n % 10;
+        // Add the digit raised to
+        // the power of k to the sum
+        sum += pow(ld, k); 
+        // Remove the last digit
+        // from the number
+        n = n / 10;
+    }
+    // Check if the sum of digits raised to
+    // the power of k equals the original number
+    return sum == number ? true : false;
+}
+
+int main(){
+    int number = 153;
+    if (isArmstrong(number)) {
+        cout << number << " is an Armstrong number." << endl;
+    } else {
+        cout << number << " is not an Armstrong number." << endl;
+    }
+    return 0;
+}
