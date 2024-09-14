dffd26bb3dc3940be41b36aabca0525ba5a4ddc9
Dhanush D <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-09-14 19:07:56 +0530

    WAP to find the total number of good pairs

--- 3_Arrays/Easy/17_Number_of_Good_Pairs/good_pairs.cpp
+++ 3_Arrays/Easy/17_Number_of_Good_Pairs/good_pairs.cpp
@@ -5,7 +5,7 @@ int numIdenticalPairs(vector<int>& nums) {
     int count =0;
     int n = nums.size();
     for(int i=0 ;i< n ; i++){
-        for(int j=i+1 ; j<nums.size() ; j++){
+        for(int j=i+1 ; j<n ; j++){
             if(nums[i]==nums[j])
             count++;
         }
