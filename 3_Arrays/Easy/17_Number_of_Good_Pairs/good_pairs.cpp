b1dee5ed2bf0565a6acbc467908fc0f0627e37a5
Dhanush D <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-09-14 19:06:57 +0530

    WAP to find the total number of good pairs

---
+++ 3_Arrays/Easy/17_Number_of_Good_Pairs/good_pairs.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numIdenticalPairs(vector<int>& nums) {
+    int count =0;
+    int n = nums.size();
+    for(int i=0 ;i< n ; i++){
+        for(int j=i+1 ; j<nums.size() ; j++){
+            if(nums[i]==nums[j])
+            count++;
+        }
+    } 
+    return count;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1, 1, 3};
+    cout << "Number of good pairs: " << numIdenticalPairs(nums) << endl;
+    return 0;
+}
