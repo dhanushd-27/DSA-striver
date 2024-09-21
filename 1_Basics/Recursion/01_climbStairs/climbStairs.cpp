dee5c6acbeef94af747be2e24e3a37b99538ef02
Dhanush D <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-09-21 22:13:45 +0530

    WAP to find the number of ways to climb to the top

---
+++ 1_Basics/Recursion/01_climbStairs/climbStairs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<int, int> cache;
+
+int climbStairs(int n) {
+    if(n == 0 || n == 1) return 1;
+
+    if(cache[n] != 0) return cache[n];
+
+    cache[n] = climbStairs(n-1) + climbStairs(n-2);
+    return climbStairs(n-1) + climbStairs(n-2);
+}
+
+int main(){
+    int stairs = 4;
+    cout<< "There are "<< climbStairs(stairs)<< " ways to climb to the top";
+    return 0;
+}
