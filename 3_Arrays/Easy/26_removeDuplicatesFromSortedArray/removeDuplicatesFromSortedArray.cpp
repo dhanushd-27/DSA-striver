18069b06656a97179ea73c9f614aaed299017b2d
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-10-05 17:03:04 +0530

    WAP to remove Duplicates from sorted array

---
+++ 3_Arrays/Easy/26_removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    int i = 0, j = 0;
+    int prev = -101;
+
+    for(; i < nums.size(); i++){
+
+        if(nums[i] != prev){
+            nums[j] = nums[i];
+            j ++;
+            prev = nums[i];
+        }
+    }
+
+    return j;
+}
+
+int main(){
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+    int unqEles = removeDuplicates(nums);
+
+    cout<< "The number of unique elements are: "<< unqEles<< endl;
+}
