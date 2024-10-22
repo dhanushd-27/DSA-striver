026792e2afd18e8fbabc12704db686b5badc2f7f
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-10-22 17:58:25 +0530

    Wap to reverse vowels in given string - Leetcode 345

---
+++ 3_Arrays/Easy/30_reverseVowels/reverseVowels.cpp
@@ -0,0 +1,42 @@
+// Leetcode - 345
+
+// Reverse vowels of a string
+
+// Find given character is a vowel or not
+bool isVowel(char c){
+    c = (char)tolower(c);
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c =='u'){
+        return true;
+    }
+
+    return false;
+}
+
+// Reverse Vowels
+string reverseVowels(string s) {
+    int n = s.length();
+    int i = 0;
+    int j = n - 1;
+
+    while(i < j){
+
+        // If s[i] is not a vowel move forward
+        if(!isVowel(s[i])){
+            i++;
+        }
+
+        // If s[j] is not a vowel reduce j value
+        else if(!isVowel(s[j])){
+            j--;
+        }
+
+        //swap both position
+        else{
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+    }
+
+    return s;
+}
