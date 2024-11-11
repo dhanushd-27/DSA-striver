e137ac6e86a02a84afb3e3a0abea945ccab202b1
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-11-11 14:58:16 +0530

    wAP to create a linked list containing all elements of given array

---
+++ 7_LinkedLists/01_Arr2List/sol.cpp
@@ -0,0 +1,60 @@
+// Given an array of integers. 
+// Create a Linked List containing all the elements of the array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC: O(n)
+// SC: O(1)
+
+// Node of a Linked List
+class Node{
+    public: 
+        int data;
+        Node* next;
+
+    public:
+        Node(int data1, Node* next1) {
+            data = data1;
+            next = next1;
+        }
+
+    public:
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
+};
+
+// Return head of the linked list
+Node* convertArrToList(vector<int> arr){
+    int size = arr.size();
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i = 1; i < size; i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+int main(){
+    vector<int> arr = { 2, 5, 8, 10, 14 };
+    Node* head = convertArrToList(arr);
+    
+    cout<< "Head data of the linked list"<< endl;
+    cout<< head->data<< " "<< endl;
+
+    // Traverse through the linked list
+    Node* current = head;
+
+    while(current){
+        cout<< current->data<< " ";
+        current = current->next;
+    }
+
+    return 0;
+}
