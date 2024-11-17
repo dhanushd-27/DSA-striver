06ed3218b47b5f54f735b0205ba7bf1d8204e67e
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-11-17 12:10:10 +0530

    108 - Convert Sorted Array to BST

---
+++ 8_BinaryTrees/Easy/04_arrayToBinaryTree/sol.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* helper(vector<int>& nums, int left, int right){
+    // something like binary search in tree using recursion
+
+    if(left > right) return NULL;
+
+    int mid = ( left + right ) / 2;
+
+    TreeNode* root = new TreeNode(nums[mid]);
+
+    root->left = helper(nums, left, mid - 1);
+    root->right = helper(nums, mid + 1, right);
+
+    return root;
+}
+
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    int size = nums.size();
+
+    int left = 0;
+    int right = size - 1;
+
+    TreeNode* root = helper(nums, left, right);
+    return root;
+}
