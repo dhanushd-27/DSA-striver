a7eba507ef60a7ff39002ec58a0007e4d849fcc8
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-11-22 21:48:33 +0530

    Leetcode 198 - House Robber

---
+++ 3_Arrays/Medium/M31_houseRobber/sol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC - O(n)
+// SC- O(n)
+
+// use memo to avoid repeatition of same values
+int t[101];
+
+int solve(vector<int> &nums, int index, int size){
+    
+    if(index >= size) return 0;
+
+    // return if the calc is already done
+    if(t[index] != -1) return t[index];
+
+    int steal = nums[index] + solve(nums, index + 2, size);
+    int skip = solve(nums, index + 1, size);
+
+    return t[index] =  max(steal, skip);
+}
+
+int rob(vector<int>& nums) {
+    int size = nums.size();
+
+    // create a set of elements with all values as -1 of size 101
+    memset(t, -1, sizeof(t));
+    return solve(nums, 0, size);
+}
